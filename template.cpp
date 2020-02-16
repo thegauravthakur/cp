1c4fb50038e51c0486540ec4dfbf907f2798619c
Gaurav Thakur <gthakur581@gmail.com>
2020-02-15 19:49:56 -0800

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,15 @@
+/*
+ * Created by Gaurav on 15 Feb,2020
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define int long long
+
+int32_t main() {
+    IOS
+    
+    return 0;
+}
